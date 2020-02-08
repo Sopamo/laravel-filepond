2050bc3b2eadf534d8045f67ee81c2635526968e
Micheal Mand <micheal@kmdwebdesigns.com>
2020-02-08 15:25:20 -0700

    Allow temp files to be stored on any disk

--- config/filepond.php
+++ config/filepond.php
@@ -13,12 +13,15 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Local Temporary Path
+    | Temporary Path
     |--------------------------------------------------------------------------
     |
     | When initially uploading the files we store them in this path
+    | By default, it is stored on the local disk which defaults to `/storage/app/{temporary_files_path}`
     |
     */
     'temporary_files_path' => env('FILEPOND_TEMP_PATH', 'filepond'),
+    'temporary_files_disk' => env('FILEPOND_TEMP_DISK', 'local'),
+
     'input_name' => 'file',
 ];
