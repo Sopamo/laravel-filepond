1ce8c032f61fafa0687abfbb5a05fba761f9566f
Simon van Thuijl <s.vanthuijl@gmail.com>
2020-10-24 11:56:03 -0400

    Routine commit

--- src/Http/Controllers/FilepondController.php
+++ src/Http/Controllers/FilepondController.php
@@ -4,6 +4,7 @@ namespace Sopamo\LaravelFilepond\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
