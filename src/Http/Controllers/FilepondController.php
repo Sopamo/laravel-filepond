35166cabeb8db2ee30beaa7f8918df65e732db0b
hossein yaghoubi <37728234+hypy13@users.noreply.github.com>
2020-12-13 16:35:54 +0330

    change absoulte path to relative path
    laravel cant delete file from absoulte path

--- src/Http/Controllers/FilepondController.php
+++ src/Http/Controllers/FilepondController.php
@@ -65,6 +65,8 @@ class FilepondController extends BaseController
     public function delete(Request $request)
     {
         $filePath = $this->filepond->getPathFromServerId($request->getContent());
+        $filePath = Str::after($tmpFilePath, "/storage/app/");
+
         if (Storage::disk(config('filepond.temporary_files_disk', 'local'))->delete($filePath)) {
             return Response::make('', 200, [
                 'Content-Type' => 'text/plain',
