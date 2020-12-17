e9da4869fd17cfe1dbb5bfcf44c64b25c3e3ea9e
hossein yaghoubi <37728234+hypy13@users.noreply.github.com>
2020-12-17 16:04:48 +0330

    add route for reload file

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Sopamo\LaravelFilepond\Http\Controllers\FilepondController;
 Route::prefix('api')->group(function () {
     Route::post('/process', [FilepondController::class, 'upload'])->name('filepond.upload');
     Route::delete('/process', [FilepondController::class, 'delete'])->name('filepond.delete');
+    Route::get('/load', [FilepondController::class, 'load'])->name('filepond.load');
+
 });
